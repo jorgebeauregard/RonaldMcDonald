c0c1ae0dc8012824ee8d618f36d027ec30349f05
Julián Huerta <fhuertaymunive@hotmail.com>
2018-10-09 03:36:26 -0500

    Added some sort of template for the controller and the api route call

--- back/McDonaldBack/app/Http/Controllers/StateController.php
+++ back/McDonaldBack/app/Http/Controllers/StateController.php
@@ -5,81 +5,21 @@ namespace App\Http\Controllers;
 use App\State;
 use Illuminate\Http\Request;
 
-class StateController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+class StateController extends Controller{
+   //to many stuff added, we only make cruds
+    public function create(Request $request){
+
+    }
+
+    public function read(Request $request){
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function update(Request $request){
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function delete(Request $request){
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\State  $state
-     * @return \Illuminate\Http\Response
-     */
-    public function show(State $state)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\State  $state
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(State $state)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\State  $state
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, State $state)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\State  $state
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(State $state)
-    {
-        //
     }
 }
--- back/McDonaldBack/routes/api.php
+++ back/McDonaldBack/routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//State
+Route::post('/state',   'StateController@create');
+Route::get('/state',    'StateController@read');
+Route::put('/state',    'StateController@update');
+Route::delete('/state', 'StateController@delete');
