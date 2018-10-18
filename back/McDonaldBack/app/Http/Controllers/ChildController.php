2d6d533b90bb8d0402dc2d4b5eeb690dd7148e83
Julián Huerta <fhuertaymunive@hotmail.com>
2018-10-18 00:15:08 -0500

    Added architecture diagram and added companions of a child query

--- back/McDonaldBack/app/Http/Controllers/ChildController.php
+++ back/McDonaldBack/app/Http/Controllers/ChildController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Child;
+use App\ChildCompanion;
+use App\Relationship;
 use Illuminate\Http\Request;
 
 class ChildController extends Controller
@@ -16,8 +18,15 @@ class ChildController extends Controller
             return Child::get();
         }
         else{
-            $id = ((int)($request->id));
-            return Child::find($id);
+            $child = Child::find($request->id);
+            if ($child == null) {
+                return response()->json(array(
+                    "error" => "id ".$request->id." not found"
+                ),404);
+            }
+            else{
+                return $child;
+            }
         }
     }
 
@@ -27,5 +36,38 @@ class ChildController extends Controller
 
     public function delete(Request $request){
 
+    }
+
+    public function getCompanions(Request $request){
+        if($request->id == null){
+            return response()->json(array(
+                "error"=>"id absent from request"
+            ),400);
+        }
+        else{
+            $child = Child::find($request->id);
+            if ($child == null) {
+                return response()->json(array(
+                    "error" => "id ".$request->id." not found"
+                ),404);
+            }
+            else{
+                $companions = $child->companions()->get();
+                foreach($companions as $companion) {
+
+                    $rel = ChildCompanion::get()->where('companion_id','=',$companion->id)->where('child_id','=',$child->id);
+                    if($rel != null){
+                        $rel_name = "";
+                        foreach($rel as $rel1){
+                            $id = $rel1->relationship_id;
+                            $relation = Relationship::find($id);
+                            $rel_name = $relation->name;
+                        }
+                        $companion->relationship_name = $rel_name;
+                    }
+                }
+                return $companions;
+            }
+        }
     }
 }
--- back/McDonaldBack/database/seeds/ChildrenTableSeeder.php
+++ back/McDonaldBack/database/seeds/ChildrenTableSeeder.php
@@ -55,6 +55,34 @@ class ChildrenTableSeeder extends Seeder
             'min_wage' => "1 a 2"
         ]);
 
+        $gender = ["Masculino","Femenino"];
+        $social = ['1','2','3','4','5','6'];
+        $zone = ['Rural', 'Sub-urbana', 'Urbana'];
+        $wage = ['<1', '1 a 2', '>2'];
+        for ($i = 0; $i <= 40; $i++) {
+            DB::table('children')->insert([
+                'names' => "child#number".$i.str_random(10),
+                'flast_name' => str_random(10),
+                'mlast_name' => str_random(10),
+                'birthday' => Carbon::parse('1995-12-30'),
+                'sex' => $gender[rand(0,1)],
+                'scholarship_id' => rand(1,9),
+                'address_street' => str_random(10),
+                'address_number' => str_random(4),
+                'neighborhood' => str_random(10),
+                'locality' => str_random(10),
+                'municipality' => str_random(10),
+                'zip_code' => str_random(7),
+                'state_id' => rand(1,33), //veracruz
+                'phone_1' => str_random(3)."-".str_random(3)."-".str_random(4),
+                'phone_2' => str_random(3)."-".str_random(3)."-".str_random(4),
+                'social' => $social[rand(0,5)],
+                'zone_type' => $zone[rand(0,2)],
+                'min_wage' => $wage[rand(0,2)],
+            ]);
+        }
+
+     
         //str_random(10).'@outlook.com'
     }
 }
